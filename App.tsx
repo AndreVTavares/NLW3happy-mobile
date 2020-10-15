cdd9fc4425b6ead8ecdc55f5a53681e0dde10121
AndreVTavares <andretavares3@gmail.com>
2020-10-15 13:34:30 -0300

    Creating the first screen with react-native-maps and adding the marker

--- App.tsx
+++ App.tsx
@@ -1,12 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
+
+import mapMarker from './src/images/map-marker.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={{
+          latitude: -3.7453417,
+          longitude: -38.4733058,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -3.7453417,
+            longitude: -38.4733058,
+          }}
+          calloutAnchor={{
+            x: 2.78,
+            y: 0.83,
+          }}
+        >
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Grace Field House</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}> 2 orfanatos encontrados</Text>
+
+        <TouchableOpacity
+          style={styles.createOrphanageButton}
+          onPress={() => {}}
+        >
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,8 +61,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#BBB',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+  },
+
+  footer: {},
+
+  footerText: {},
+
+  createOrphanageButton: {},
 });
